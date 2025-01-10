c3b7ec75824ebc36aa9f8f4077b94c5a34f457e2
Kathinka <kathinbs@stud.ntnu.no>
2025-01-10 15:03:41 +0100

    Endret på foo.c og foo.go slik at resultatet blir 0

--- shared variable/c/foo.c
+++ shared variable/c/foo.c
@@ -6,11 +6,21 @@
 
 int i = 0;
 
+// Mutex for synchronization
+pthread_mutex_t lock;
+/*
+Why Use a Mutex (pthread_mutex_t) Instead of a Semaphore?
+A mutex is specifically designed to provide mutual exclusion for shared resources, making it the ideal choice for protecting the shared variable i.
+Semaphores are more general and are better suited for signaling and managing access to multiple resources.
+In this case, only one thread should modify i at a time, so a mutex is the most efficient and appropriate solution.
+*/
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
     for (int j = 0; j < 1000000; j++) {
-        i++;
+        pthread_mutex_lock(&lock);  // Acquire the lock
+        i++;                        // Critical section
+        pthread_mutex_unlock(&lock);  // Release the lock
     }
     return NULL;
 }
@@ -18,7 +28,9 @@ void* incrementingThreadFunction(){
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
     for (int j = 0; j<1000000; j++) {
-        i--;
+        pthread_mutex_lock(&lock);  // Acquire the lock
+        i--;                        // Critical section
+        pthread_mutex_unlock(&lock);  // Release the lock
     }
     return NULL;
 }
@@ -31,6 +43,9 @@ int main(){
     
     //Deklarer to variabler
     pthread_t thread1, thread2;
+
+    // Initialize the mutex
+    pthread_mutex_init(&lock, NULL);
     // Create threads
     pthread_create(&thread1, NULL, incrementingThreadFunction, &i);
     pthread_create(&thread2, NULL, decrementingThreadFunction, &i);
@@ -42,7 +57,11 @@ int main(){
  // pthread_join passer på at begge to treadsene er ferdige før man går videre. 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL); 
+
     
     printf("The magic number is: %d\n", i);
+   
+    // Destroy the mutex
+    pthread_mutex_destroy(&lock);
     return 0;
 }
--- shared variable/go/foo.go
+++ shared variable/go/foo.go
@@ -5,36 +5,76 @@ package main
 import (
     . "fmt"
     "runtime"
-    "time"
 )
 
-var i = 0
+// Definerer hvilke kommandoer serveren kan motta
+type Command string
 
-func incrementing() {
-    for j:=0; j<1000000; j++ {
-        i++;
-    }
-    //TODO: increment i 1000000 times
-}
+const (
+    Increment Command = "inc"  // Øker verdien til i
+    Decrement Command = "dec"  //
+    Get      Command = "get"  //Henter verdien til i
+)
+//Må gi de en string-verdi sånn at de ikke er like..... skjønner ikke helt. 
 
-func decrementing() {
-    //TODO: decrement i 1000000 times
-    for j:=0; j<1000000; j++ {
-        i--;
+// Server-funksjonen som håndterer all tilgang til variabelen i
+func numberServer(cmdChan <-chan Command, resultChan chan<- int) {
+    i := 0  // Den delte variabelen håndteres bare av serveren
+
+    for {
+        select {
+        case cmd := <-cmdChan: //cmdChan = Kanal som skal motta ulike kommandoer, som inc, dec osv..
+                                //Når cmdChan mottar en verdi/kommando blir denne lagret i cmd. 
+                                // Sjekker så under hvilken kommando dette er.  
+            switch cmd {
+            case Increment:
+                i++
+            case Decrement:
+                i--
+            case Get:   //Get skal hente verdien til i. 
+                        //Sender verdien til i til kanalen "resultChan":
+                resultChan <- i  // Sender verdien tilbake til main
+                return           // Avslutter serveren etter å ha sendt verdien
+            }
+        }
     }
 }
 
 func main() {
-    // What does GOMAXPROCS do? What happens if you set it to 1?
-    runtime.GOMAXPROCS(2)    
-    // Setter antall GPU kjerner til 2
-    // Hvis man setter det til 1 vil bare en goroutine kunne kjøre samtidig!
-	
-    // TODO: Spawn both functions as goroutines
-	go incrementing()
-    go decrementing()
-    // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
-    // We will do it properly with channels soon. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
-    Println("The magic number is:", i)
+    runtime.GOMAXPROCS(2)  // Tillater programmet å bruke to CPU-kjerner
+
+    cmdChan := make(chan Command)  // Kanal for kommandoer til serveren
+    resultChan := make(chan int)   // Kanal for å hente resultatet
+    done := make(chan bool)        // Kanal for å signalere at en goroutine er ferdig
+
+    // Starter serveren som håndterer i
+    go numberServer(cmdChan, resultChan)
+
+    // Goroutine for å inkrementere i
+    go func() {
+        for j := 0; j < 1000000; j++ {
+            cmdChan <- Increment        // Istedet for å skrive "i++", sender vi kanalen "Increment" til cmdChan. 
+                                        // Da vil numberServer "kjøre". Switchen vil altså fungere når cmdChan blir tilsendt en kanal/verdi. 
+        }
+        done <- true  // Signaliserer at inkrementering er ferdig
+        // done er kanalen som passer på om en gorutine er ferdig eller ikke. Setter den til å være true. 
+    }()
+
+    // Goroutine for å dekrementere i
+    go func() {
+        for j := 0; j < 1000000; j++ {
+            cmdChan <- Decrement
+        }
+        done <- true  // Signaliserer at dekrementering er ferdig
+    }()
+
+    // Venter på at begge goroutines er ferdige
+    <-done
+    <-done  //done blokkerer programmet helt til det mottar noe. Så når det mottar "true" går vi videre. 
+
+    // Sender en "Get"-kommando for å hente sluttverdien av i
+    cmdChan <- Get  // Kommandoen Get blir sendt, og resultChan får da verdien til i. 
+    result := <-resultChan  //Lagrer i i result. 
+
+    Println("The magic number is:", result)
 }
